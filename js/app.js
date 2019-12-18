c278886d0d44e18cfc9ef5d751302e0e7e00efcf
Jaspreet Singh -- fearless <jaspreetsingh2379@gmail.com>
2019-12-18 23:41:21 +0530

    Added  ios Support, Fetch evt in SW

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-if('serviceWorker' in navigator){
-  
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(x => {
+      console.log("Service Worker Registered");
+      // console.log(x);
+    })
+    .catch(err => {
+      console.log("Service Worker Not Registered", err);
+    });
 }
--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-// Created in root for overall scope
+// Created in root for root scope i.e access to all Files
+
+// Fired upon when service worker is installed
+self.addEventListener('install', evt => {
+  console.log("Service Worker is installed", evt)
+})
+
+// Fired upon when service worker is activated
+self.addEventListener('activate', evt => {
+  console.log("Service Worker is activated", evt)
+})
+
+// Fetch...
+self.addEventListener('fetch', evt => {
+  console.log("Fetch Event: ", evt)
+})
