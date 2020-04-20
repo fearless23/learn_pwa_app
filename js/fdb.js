20b94f469a0000cc4e1ceb801f3016d01569da98
Jaspreet Singh -- fearless <jaspreetsingh2379@gmail.com>
2020-04-20 07:04:08 +0530

    Added New Things

--- js/fdb.js
+++ js/fdb.js
@@ -46,11 +46,11 @@ const login = async function(userEmail, password) {
 
 const googleLogin = async function() {
   const provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+  provider.addScope("https://www.googleapis.com/auth/userinfo.profile");
   try {
     const result = await fbAuth.signInWithPopup(provider);
     // const result = await fbAuth.signInWithRedirect(provider);
-    console.log(result)
+    console.log(result);
     var token = result.credential.accessToken;
   } catch (error) {
     console.log(error);
@@ -59,9 +59,62 @@ const googleLogin = async function() {
 
 const createUserEP = async function(userEmail, password) {
   try {
-    const result = await createUserWithEmailAndPassword(email, password);
+    const result = await fbAuth.createUserWithEmailAndPassword(
+      userEmail,
+      password
+    );
     console.log(result);
   } catch (error) {
     console.log(error);
+  }
+};
+
+const getPhoneNumberFromUserInput = function() {
+  const mobNumberInput = document.getElementById("mobNumber");
+  return mobNumberInput.value;
+};
+
+const getCodeFromUserInput = function() {
+  const codeInput = document.getElementById("code");
+  return codeInput.value;
+};
+
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+  "recaptcha-container",
+  {
+    size: "normal",
+    callback: () => phoneLoginInternal(),
+    "expired-callback": () => {
+      console.log("CALLBACK EXPIRED");
+    }
+  }
+);
+
+const mobileLogin = async function() {
+  await recaptchaVerifier.render();
+};
+
+const phoneLoginInternal = async function() {
+  try {
+    const phoneNumber = getPhoneNumberFromUserInput();
+    // var recaptchaResponse = grecaptcha.getResponse(window.recaptchaWidgetId);
+    // console.log("RCR: ", recaptchaResponse);
+    window.captchaResult = await fbAuth.signInWithPhoneNumber(
+      phoneNumber,
+      window.recaptchaVerifier
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const putCode = async function() {
+  // qwerty
+  try {
+    const code = getCodeFromUserInput();
+    const result = await window.captchaResult.confirm(code);
+    console.log("FINAL", result);
+  } catch (err) {
+    console.log(err);
   }
 };
